472d7d146f2baa2f9a2bc4303ec3ea1581bed2d0
ht155 <haoyang.tong@duke.edu>
2022-07-18 20:24:58 -0400

    newest version

--- histFMVV4.py
+++ histFMVV4.py
@@ -5,11 +5,13 @@ Created on Tue Jul 12 17:59:01 2022
 @author: Crypto
 """
 import time
+import calendar
+
 import pandas as pd
 import numpy as np
 import requests
 import json
-import datetime
+from datetime import datetime, timedelta
 import pytz
 import aiohttp
 import asyncio
@@ -24,18 +26,19 @@ async def lookupFMV_Kaiko(date, token_symbol, before, after):
     price_dict_key = date + '-' + token_symbol
     if price_dict_key in price_dict:
         return price_dict[price_dict_key]
-    Time = datetime.datetime.strptime(str(date), '%Y-%m-%dT%H:%M:%SZ')
-    start_time = Time - datetime.timedelta(hours=before)
+    target_time = datetime.strptime(str(date), '%Y-%m-%dT%H:%M:%SZ')
+    start_time = target_time - timedelta(hours=before)
     start_time = str(start_time.strftime('%Y-%m-%dT%H:%M:%SZ'))
-    end_time = Time + datetime.timedelta(hours=after)
+    end_time = target_time + timedelta(hours=after)
     end_time = str(end_time.strftime('%Y-%m-%dT%H:%M:%SZ'))
-    timestamp = int(datetime.datetime.timestamp(Time))
-    tx_url = "https://us.market-api.kaiko.io/v1/data/trades.v1/spot_exchange_rate/" + \
-             str(token_symbol).lower() + "/usd?interval=1m&page_size=1000"
-    params = dict({"start_time": start_time, "end_time": end_time})
+    tx_url = "https://us.market-api.kaiko.io/v1/data/trades.v1/spot_exchange_rate/{}/usd"
+    params = dict({"start_time": start_time,
+                   "end_time": end_time,
+                   "interval": "1m",
+                   "page_size": 1000})
     headers = {'x-api-key': 'b45d6e160228c4514fd747d8ab16cd08'}
     async with aiohttp.ClientSession(headers=headers) as session:
-        async with session.get(tx_url, headers=headers, params=params) as response:
+        async with session.get(tx_url.format(str(token_symbol).lower()), headers=headers, params=params) as response:
             text = await response.text()
             if response.status != 200:
                 if response.status == 400:
@@ -47,12 +50,8 @@ async def lookupFMV_Kaiko(date, token_symbol, before, after):
             json_data = (json.loads(text))
             fmv = pd.DataFrame(json_data['data'])
             fmv = fmv[~fmv['price'].isna()]
-            fmv['datetime'] = ''
             fmv['delta'] = 0.0
-            Time = Time.replace(tzinfo=pytz.utc)
-            for i in fmv.index:
-                fmv.loc[i, 'datetime'] = datetime.datetime.fromtimestamp(int(fmv['timestamp'][i]) / 1000, tz=pytz.utc)
-                fmv.loc[i, 'delta'] = abs(float((fmv['datetime'][i] - Time).total_seconds() / 3600))
+            fmv['delta'] = ((fmv['timestamp']/1000 - calendar.timegm(target_time.timetuple()))/3600).apply(abs)
             price = fmv['price'][fmv['delta'] == fmv['delta'].min()].to_list()[0]
             delta = fmv['delta'].min()
             if abs(delta) < 1:
@@ -63,29 +62,6 @@ async def lookupFMV_Kaiko(date, token_symbol, before, after):
             return price, delta
 
 
-async def add_fmv(data, row_index, interval=0.5):
-    global incomplete_set
-    row = data.iloc[row_index]
-    incomplete_set.remove(row_index)
-    if row['txType'] in ['Convert']:
-        return
-    try:
-        if (not pd.isna(row['debitAsset'])) and ('-' not in row['debitAsset']):
-            data.loc[row_index, 'debitAsset_FMV'], _ = await lookupFMV_Kaiko(row['datetime'], row['debitAsset'],
-                                                                             interval,
-                                                                             interval)
-        if (not pd.isna(row['creditAsset'])) and row['creditAsset'] != 'USD' and ('-' not in row['creditAsset']):
-            data.loc[row_index, 'creditAsset_FMV'], _ = await lookupFMV_Kaiko(row['datetime'], row['creditAsset'],
-                                                                              interval,
-                                                                              interval)
-        if (not pd.isna(row['txFeeAsset'])) and ('-' not in row['txFeeAsset']):
-            data.loc[row_index, 'txFeeAsset_FMV'], _ = await lookupFMV_Kaiko(row['datetime'], row['txFeeAsset'],
-                                                                             interval,
-                                                                             interval)
-    except Exception as err:
-        incomplete_set.add(row_index)
-
-
 async def main(task_list):
     await asyncio.gather(*task_list)
 
@@ -93,21 +69,22 @@ async def main(task_list):
 async def get_fmv(lookup, row_index, window=1):
     global incomplete_set
     row = lookup.iloc[row_index]
-    incomplete_set.remove(row_index)
     try:
         lookup.loc[row_index, 'fmv'], _ = await lookupFMV_Kaiko(row['datetime'], row['token_symbol'], window / 2,
                                                                 window / 2)
     except Exception as err:
         incomplete_set.add(row_index)
         lookup.loc[row_index, 'fmv'] = -1
-        # print(err)
+        print(row['fmv_id'] + ': ' + str(err))
+    else:
+        incomplete_set.remove(row_index)
 
 
 def execute(file_name, maximum_attempt=5):
     global incomplete_set
     attempts = 0
-    window = 1
-    error_window = 16
+    default_window = 1
+    default_error_window = 16
     start_time = time.perf_counter()
     data = pd.read_csv(file_name)
     data['debitAssetId'] = data['datetime'] + '-' + data['debitAsset']
@@ -124,14 +101,13 @@ def execute(file_name, maximum_attempt=5):
     end = len(lookup)
     incomplete_set = set([i for i in range(start, end)])
     loop = asyncio.get_event_loop()
-    task_list = [loop.create_task(get_fmv(lookup, i)) for i in range(start, end)]
+    task_list = [loop.create_task(get_fmv(lookup, i, window=default_window)) for i in range(start, end)]
     loop.run_until_complete(main(task_list))
     while len(incomplete_set) > 0 and attempts < maximum_attempt:
-        task_list = [loop.create_task(get_fmv(lookup, i, window=16)) for i in incomplete_set]
+        task_list = [loop.create_task(get_fmv(lookup, i, window=default_error_window)) for i in incomplete_set]
         loop.run_until_complete(main(task_list))
         attempts += 1
     loop.close()
-   # print(lookup)
     print('total time: ' + str(time.perf_counter() - start_time))
     print('Failures Remained: ' + str(len(incomplete_set)))
     start_time = time.perf_counter()
@@ -145,7 +121,7 @@ def execute(file_name, maximum_attempt=5):
     data.rename(columns={'fmv': 'txFeeAssetFMV'}, inplace=True)
     data.drop(columns=['fmv_id', 'creditAssetId', 'debitAssetId', 'txFeeAssetId'], inplace=True, axis=1)
     data.to_csv('FMV_output.csv')
-    print('Time to merger data: ' + str(time.perf_counter() - start_time))
+    print('Time to merge data: ' + str(time.perf_counter() - start_time))
 
 if __name__ == '__main__':
     file_name = 'test_download.csv'
