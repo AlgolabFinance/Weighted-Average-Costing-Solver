c44826b38e4c58bca4e2ba15f909e5c43ef9729b
ht155 <haoyang.tong@duke.edu>
2022-08-01 17:54:15 -0400

    newest version

--- quickbook/cgl_iif.py
+++ quickbook/cgl_iif.py
@@ -1,17 +1,50 @@
+import glob
+import os
+
+import numpy as np
 import pandas as pd
 import calendar
+import glob
 
-def get_grouped_cgl(file_name, sheet_name):
+# def get_grouped_cgl(file_name, sheet_name):
+#     data = pd.read_excel(file_name, sheet_name=sheet_name)
+#     data['month'] = data['Date sold or disposed of'].astype(str).str[0:7]
+#     for i, row in data.iterrows():
+#         asset = row['Description of property'].split('-')[0]
+#         data.loc[i, 'asset'] = asset
+#     data = data.groupby(['month', 'asset'], as_index=False)['Gain or (loss)'].sum()
+#     data['account'] = '11000 Cryptocurrency:' + data['asset']
+#     data['opp_account'] = '80000 Crypto Gains/(Losses):Crypto - Capital Gains & Losses:' \
+#                           + data['asset'] + ' - Net Capital Gain & Loss'
+#     return data
+
+def get_grouped_cgl(file_name, sheet_name=0):
     data = pd.read_excel(file_name, sheet_name=sheet_name)
+    if data.empty:
+        print('Empty File: ' + file_name)
+        return None, None
+    data['class'] = np.NAN
+    long_term_index = data[data['Description of property'] == 'Long Term'].index.tolist()[0]
+    short_term_index = data[data['Description of property'] == 'Short Term'].index.tolist()[0]
+    if long_term_index < short_term_index:
+        data.loc[long_term_index:, 'class'] = 'Long Term'
+        data.loc[short_term_index:, 'class'] = 'Short Term'
+    else:
+        data.loc[short_term_index:, 'class'] = 'Short Term'
+        data.loc[long_term_index:, 'class'] = 'Long Term'
+    data = data[~data['Date sold or disposed of'].isna()]
     data['month'] = data['Date sold or disposed of'].astype(str).str[0:7]
     for i, row in data.iterrows():
         asset = row['Description of property'].split('-')[0]
         data.loc[i, 'asset'] = asset
-    data = data.groupby(['month', 'asset'], as_index=False)['Gain or (loss)'].sum()
+    data = data.groupby(['month', 'asset', 'class'], as_index=False)['Gain or (loss)'].sum()
     data['account'] = '11000 Cryptocurrency:' + data['asset']
     data['opp_account'] = '80000 Crypto Gains/(Losses):Crypto - Capital Gains & Losses:' \
                           + data['asset'] + ' - Net Capital Gain & Loss'
-    return data
+    long_term_data = data[data['class'] == 'Long Term']
+    short_term_data = data[data['class'] == 'Short Term']
+    return short_term_data, long_term_data
+
 
 def generate_monthly_iif(cgl_df):
     iif = pd.DataFrame({'specifier': [], 'date': [], 'tx_type': [],
@@ -28,7 +61,6 @@ def generate_monthly_iif(cgl_df):
             specifier = 'TRANS'
         else:
             specifier = 'SPL'
-        print(row)
         day = calendar.month(int(row['month'][0:4]), int(row['month'][-2:]))[-3:-1]
         date = row['month'].replace('-', '/') + '/' + day
         account = row['account']
@@ -51,17 +83,18 @@ def generate_monthly_iif(cgl_df):
 
 
 def convert_cgl_to_iif(file_name):
-    short_term = get_grouped_cgl(file_name, 'Short Term')
-    long_term = get_grouped_cgl(file_name, 'Long Term')
+    short_term, long_term = get_grouped_cgl(file_name)
+    if short_term is None:
+        return None, None
     iif_header = pd.DataFrame({'specifier': ['!TRNS', '!SPL', '!ENDTRANS'], 'date': ['DATE', 'DATE', ''],
                                'tx_type': ['TRNSTYPE', 'TRNSTYPE', ''],
                                'doc_num': ['DOCNUM', 'DOCNUM', ''], 'account': ['ACCNT', 'ACCNT', ''],
-                               'amount': ['AMOUNT', 'AMOUNT', ''], 'memo': ['MEMO', 'MEMO', '']})
+                               'amount': ['AMOUNT', 'AMOUNT', ''], 'memo': ['MEMO', 'MEMO', ''],
+                               'name': ['NAME', 'NAME', '']})
     st_month_list = short_term['month'].drop_duplicates().to_list()
     lt_month_list = long_term['month'].drop_duplicates().to_list()
     short_term_iif = iif_header.copy()
     long_term_iif = iif_header.copy()
-    #print(short_term)
     for month in st_month_list:
         month_data = short_term[short_term['month'] == month]
         month_iif = generate_monthly_iif(month_data)
@@ -77,11 +110,23 @@ def convert_cgl_to_iif(file_name):
         long_term_iif.to_excel(writer, sheet_name='Long Term IIF', index=False, header=False)
     return long_term_iif, short_term_iif
 
+    # else:
+    #     for file_name in file_list:
+    #         prefix = file_name.split('.')[-2]
+    #         long_term_iif, shor_term_iif = convert_cgl_to_iif(file_name)
+    #         if long_term_iif is not None:
+    #             long_term_iif.to_csv(prefix + '_long_term.iif', sep='\t')
+    #         if shor_term_iif is not None:
+    #             shor_term_iif.to_csv(prefix + '_short_term.iif', sep='\t')
 
-if __name__ == '__main__':
-    path = '/Users/tonghaoyang/PycharmProjects/Weighted-Average-Costing-Solver2/quickbook/'
-    pd.set_option('display.max_columns', None)
-    file_name = path + '2022 FIFO.xlsx'
-    long_term_iif, short_term_iif = convert_cgl_to_iif(file_name)
-    long_term_iif.to_csv('long_term_iif.iif', sep='\t')
-    short_term_iif.to_csv('short_term_iif.iif', sep='\t')
+
+# if __name__ == '__main__':
+#     path = '/Users/tonghaoyang/PycharmProjects/Weighted-Average-Costing-Solver2/quickbook/'
+#     pd.set_option('display.max_columns', None)
+#     # file_name = path + '2022 FIFO.xlsx'
+#     # file_name2 = path + '8949/' + '62e450d070fa9d1da684b2a7_2022_FIFO.xlsx'
+#     # long_term_iif, short_term_iif = convert_cgl_to_iif(file_name2)
+#     # long_term_iif.to_csv('long_term_iif.iif', sep='\t')
+#     # short_term_iif.to_csv('short_term_iif.iif', sep='\t')
+#     convert_all_files(path + '8949/', True)
+
